0f9d2e5b1177da61a856bc2630964c3225c7579f
Stefan Adams <stefan.adams@vipcomag.de>
2019-12-06 15:04:29 +0100

    - code cleanup - use solid black - add #clear command

--- src/demos/nanox/nxdisplay.c
+++ src/demos/nanox/nxdisplay.c
@@ -13,52 +13,65 @@ Version 0.1 14.06.2018
 #include "nxcolors.h"
 
 #define DEFAULT_DATA_FILE "/etc/nxcal.dat"
-#define MAXLINES 20
-#define DIST     15
-#define MINPOS   0
-#define LINEWIDTH 80
+#define MAXLINES  20
+#define DIST      15
+#define MINPOS    0
+#define LINEWIDTH 90
 #define FIFO "/tmp/zsiposfifo"
 
-static GR_WINDOW_ID g_wid;
+static GR_WINDOW_ID   g_wid;
 static GR_SCREEN_INFO g_si;
+static GR_GC_ID       g_gc;
+static int            g_row = 0;
 
-static int g_rotate = MWPORTRAIT_NONE;
-
-void
-draw_text(char *str, int row, GR_GC_ID gc)
+static void clear_screen()
 {
-    int tw, th, tb;
+    GrSetGCForeground(g_gc, GR_COLOR_BLACK);
+    GrFillRect(g_wid, g_gc, 0, 0, g_si.cols, g_si.rows);
 
-    //GrGetGCTextSize(gc, str, -1, GR_TFTOP, &tw, &th, &tb);
-    GrText(g_wid, gc, 20, row, str, -1, GR_TFTOP);
+    GrSetGCForeground(g_gc, GR_COLOR_WHITE);
+    GrSetGCBackground(g_gc, GR_COLOR_BLACK);
+    g_row = 0;
 }
 
-void nextline(char *str, GR_GC_ID gc)
+static void draw_text(char *str, int row)
 {
-  static int row = 0;
-  char mytext[LINEWIDTH+1];
-  char myclear[LINEWIDTH*2+1];
-  int pos;
+    GrText(g_wid, g_gc, 20, row, str, -1, GR_TFTOP);
+}
 
-  // clear following line
-  sprintf(myclear, "%*s", LINEWIDTH*2, " ");
-  pos = ((row + 1) % MAXLINES) * DIST; 
-  draw_text(myclear, pos, gc);
-  // write line
-  sprintf(mytext, "%.*s", LINEWIDTH, str);
-  pos = row * DIST;
-  draw_text(mytext, pos, gc);
-  // increment row
-  row = (row + 1) % MAXLINES; 
- }
+static void nextline(char *str)
+{
+	char mytext[LINEWIDTH + 1];
+	char myclear[LINEWIDTH * 2 + 1];
+	int pos;
+
+	if (strcmp(str, "#clear\n") == 0) {
+		clear_screen();
+		return;
+	}
+
+	// clear following line
+	sprintf(myclear, "%*s", LINEWIDTH * 2, " ");
+	if ((g_row + 1) < MAXLINES) {
+		pos = (g_row + 1) * DIST;
+		draw_text(myclear, pos);
+	}
+	// write line
+	sprintf(mytext, "%.*s", LINEWIDTH, str);
+	pos = g_row * DIST;
+	draw_text(mytext, pos);
+	// increment row
+	g_row += 1;
+	if (g_row == MAXLINES)
+		g_row = 0;
+}
 
 int
 main(int argc, char **argv)
 {
-    int ret = 0;
-    int count = 0;
-    FILE *fp;
-    char text[LINEWIDTH+1];
+    GR_EVENT event;
+    FILE    *fp;
+    char     text[LINEWIDTH+1];
 
     if (GrOpen() < 0) {
         fprintf(stderr, "Couldn't connect to Nano-X server\n");
@@ -66,26 +79,13 @@ main(int argc, char **argv)
     }
 
     GrGetScreenInfo(&g_si);
-    g_rotate = g_si.portrait;
-    g_wid = GrNewWindowEx(GR_WM_PROPS_NODECORATE, "nxcal", GR_ROOT_WINDOW_ID,
-        0, 0, g_si.cols, g_si.rows, GR_COLOR_BLACK);
-    GrSelectEvents(g_wid, GR_EVENT_MASK_EXPOSURE);
-    GrMapWindow(g_wid);
-    // warte auf startschuss
-    while (1) {
-        GR_EVENT event;
-        GrGetNextEvent(&event);
+	g_wid = GrNewWindowEx(GR_WM_PROPS_NODECORATE, "nxdisplay",
+			GR_ROOT_WINDOW_ID, 0, 0, g_si.cols, g_si.rows, GR_COLOR_BLACK);
+	GrSelectEvents(g_wid, GR_EVENT_MASK_EXPOSURE);
+	GrMapWindow(g_wid);
 
-        if (event.type == GR_EVENT_TYPE_EXPOSURE) {
-            break;
-        }
-    }
-    GR_GC_ID gc = GrNewGC();
-    GrSetGCForeground(gc, MWRGB(30, 30, 30));
-    GrFillRect(g_wid, gc, 0, 0, g_si.cols, g_si.rows);
-
-    GrSetGCForeground(gc, GR_COLOR_WHITE);
-    GrSetGCBackground(gc, MWRGB(30, 30, 30));
+    g_gc = GrNewGC();
+    clear_screen();
 
     unlink(FIFO); // ignore error
     while (1) {
@@ -99,11 +99,13 @@ main(int argc, char **argv)
                 break;
             }
         }
-        while (fgets(text, LINEWIDTH, fp))
-            nextline(text, gc);
+        while (fgets(text, LINEWIDTH, fp)) {
+            nextline(text);
+            GrCheckNextEvent(&event);
+        }
         fclose(fp);
     }
-    GrDestroyGC(gc);
+    GrDestroyGC(g_gc);
     GrClose();
     return 0;
 }
